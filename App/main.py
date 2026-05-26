dc9bdceadce7668e548b7cd50edcb270ca8f5b9a
TigerShark323 <30355455+TigerShark323@users.noreply.github.com>
2026-05-26 12:31:51 +0000

    Pending changes exported from your codespace

--- App/main.py
+++ App/main.py
@@ -9,7 +9,14 @@ else:
 word = input ("Введите фразу для поиска:")
 data = result.json()
 def search (word, data):
+    find_res = [] 
     for c in data:
         if word in c["title"]:
-            print ("данное слово содержится в тайтле поста", c["id"], "\n", "содержание поста:", c)
-search(word, data)
+          find_res.append(c) 
+    return find_res
+posts = search (word, data)
+def print_res (posts):
+    for export in posts:
+        print ("слово найдено в порте с id:", export["id"], "\nсодержит:", "\nid:", export["id"], "\nuserId:", export["userId"], "\nTitle:", export["title"], "\nBody:", export["body"], "\n")
+    return print_res (posts)
+print_res (posts)
